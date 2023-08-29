548c1e676c3441933ff015348bbd4e916401010c
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-08-29 19:25:35 +0200

    Add validation to title, author, description

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -1,46 +1,63 @@
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { useState } from 'react';
+import { useForm } from "react-hook-form";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const PostForm = ({ action, actionText, ...props }) => {
+
   const [title, setTitle] = useState(props.title || '');
   const [author, setAuthor] = useState(props.author || '');
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
   const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
   const [content, setContent] = useState(props.content || '');
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
 
   const handleSubmit = e => {
-    e.preventDefault();
     action({ title, author, publishedDate, shortDescription, content });
   };
  
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={validate(handleSubmit)}>
       <Row>
         <Col xs='12' md='6' lg='6' >
-          <Form.Group className="mb-3" controlId="formTitle">
-            <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)} />
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            {...register("title", { required: true, minLength: 3 })}
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            type="text" placeholder="Enter title"
+          />
+          {errors.title && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formAuthor">
             <Form.Label>Author</Form.Label>
-            <Form.Control type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)} />
+            <Form.Control 
+              {...register("author", { required: true, minLength: 3 })}
+              type="text" placeholder="Enter author" 
+              value={author} 
+              onChange={e => setAuthor(e.target.value)} />
+              {errors.author && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formPublished">
             <Form.Label>Published</Form.Label>
             <DatePicker selected={publishedDate} onChange={(date) => setPublishedDate(date)} placeholder="Enter date" value={publishedDate}/>
-            
           </Form.Group>
         </Col>
       </Row>
       <Form.Group className="mb-3" controlId="formShort">
         <Form.Label>Short description</Form.Label>
-        <Form.Control as="textarea" placeholder="Live a coment here." rows={3} value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
+        <Form.Control 
+          {...register("description", { required: true, minLength: 20 })}
+          as="textarea" placeholder="Live a coment here." rows={3} 
+          value={shortDescription} 
+          onChange={e => setShortDescription(e.target.value)} />
+          {errors.description && <small className="d-block form-text text-danger mt-2">Title is too short (min is 20)</small>}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formContent">
